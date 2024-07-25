b9be887051c795c7303f455202b295cb8fa15bd1
Javier Godoy <javiergodoymiguez@gmail.com>
2024-07-25 19:08:14 +0200

    javi lab solved

--- src/pages/BeerDetailsPage.jsx
+++ src/pages/BeerDetailsPage.jsx
@@ -15,15 +15,31 @@ function BeerDetailsPage() {
   if (!beer) return <div>Loading...</div>;
 
   return (
-    <div className="container mx-auto p-4">
-      <img src={beer.image_url} alt={beer.name} className="h-80 mx-auto" />
-      <h1 className="text-3xl font-bold">{beer.name}</h1>
-      <p className="text-lg text-gray-500">{beer.tagline}</p>
-      <p><strong>First Brewed:</strong> {beer.first_brewed}</p>
-      <p className='text-gray-500'> {beer.attenuation_level}</p>
-      <p>{beer.description}</p>
-      <p className='text-gray-500'>Created by: {beer.contributed_by}</p>
+    // <div className="container mx-auto p-4">
+    //   <img src={beer.image_url} alt={beer.name} className="h-80 mx-auto" />
+    //   <h1 className="text-3xl font-bold">{beer.name}</h1>
+    //   <p className="text-lg text-gray-500">{beer.tagline}</p>
+    //   <p><strong>First Brewed:</strong> {beer.first_brewed}</p>
+    //   <p className='text-gray-500'> {beer.attenuation_level}</p>
+    //   <p>{beer.description}</p>
+    //   <p className='text-gray-500'>Created by: {beer.contributed_by}</p>
+    // </div>
+    <div className="container mx-auto p-4 bg-gray-50">
+    <img src={beer.image_url} alt={beer.name} className="h-80 mx-auto" />
+    <div className="mt-4 flex justify-between items-start">
+      <div className="flex flex-col">
+        <h1 className="text-3xl font-bold">{beer.name}</h1>
+        <p className="text-lg text-gray-600">{beer.tagline}</p>
+      </div>
+      <div className='text-xl'>
+        <p className="font-bold text-gray-600">{beer.attenuation_level}</p>
+        <p className="mt-2 font-bold"></p>
+        <p>{beer.first_brewed}</p>
+      </div>
     </div>
+    <p className="mt-2">{beer.description}</p>
+    <p className="mt-2 text-gray-600"><strong>Contributed by:</strong> {beer.contributed_by}</p>
+  </div>
   );
 }
 
