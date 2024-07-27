d457c54aa9c0ddae9168257e0374a3a139ae4405
Javier Godoy <javiergodoymiguez@gmail.com>
2024-07-27 10:09:22 +0200

    javi lab solved

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -25,7 +25,7 @@ function AddBeerPage() {
       .then(response => response.json())
       .then(data => {
         console.log(data);
-        alert("Cerveza añadida con éxito");
+        alert("Beer successfully added");
       })
       .catch(error => console.error(error));
   };
